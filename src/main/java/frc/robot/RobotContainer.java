a936535c1a926baa48af533c363f68d519cf272d
NJonathanJiang <theNerdyMango@gmail.com>
2025-01-24 21:32:37 -0600

    Added code to test the ratchet in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,11 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.util.sendable.Sendable;
@@ -14,25 +19,52 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.lib.oi.OI;
 import frc.robot.constants.Controls;
 import frc.robot.subsystems.climber.ClimberSubsystem;
+import frc.robot.subsystems.climber.RatchetMotor;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
 
 
 public class RobotContainer {
     private final OI oi = OI.getInstance();
-    private final CommandSwerveDrivetrain swerve = CommandSwerveDrivetrain.getInstance();
-    private final SendableChooser<Command> autoChooser;
+//    private final CommandSwerveDrivetrain swerve = CommandSwerveDrivetrain.getInstance();
+//    private final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
         configureBindings();
-        autoChooser = AutoBuilder.buildAutoChooser();
-        SmartDashboard.putData(autoChooser);
+//        autoChooser = AutoBuilder.buildAutoChooser();
+//        SmartDashboard.putData(autoChooser);
     }
 
     private void configureBindings() {
+        SparkMax motor = new SparkMax(1, SparkLowLevel.MotorType.kBrushless);
+        motor.configure(
+            new SparkMaxConfig().idleMode(SparkBaseConfig.IdleMode.kBrake),
+            SparkBase.ResetMode.kNoResetSafeParameters,
+            SparkBase.PersistMode.kNoPersistParameters
+        );
+        RatchetMotor ratchetMotor = new RatchetMotor(motor, 8, RatchetMotor.RatchetDirection.Forwards);
+
+        oi.driverController().POV_UP.whileTrue(new StartEndCommand(
+            () -> ratchetMotor.set(.5),
+            () -> ratchetMotor.set(0)
+        ));
+        oi.driverController().POV_DOWN.whileTrue(new StartEndCommand(
+            () -> ratchetMotor.set(-1),
+            () -> ratchetMotor.set(0)
+        ));
+
+        new SubsystemBase() {
+            @Override
+            public void periodic() {
+                ratchetMotor.periodic();
+            }
+        };
+
         /*OI.getInstance().driverController().A_BUTTON.whileTrue(
             swerve.sysIdQuasistatic(Direction.kForward)
         );
@@ -45,10 +77,10 @@ public class RobotContainer {
         OI.getInstance().driverController().Y_BUTTON.whileTrue(
             swerve.sysIdDynamic(Direction.kReverse)
         );*/
-        swerve.setDefaultCommand(swerve.applyRequest(swerve::fieldCentricRequestSupplier));
-        Controls.Driver.rotationResetTrigger.onTrue(
-            swerve.resetPigeonCommand()
-        );
+//        swerve.setDefaultCommand(swerve.applyRequest(swerve::fieldCentricRequestSupplier));
+//        Controls.Driver.rotationResetTrigger.onTrue(
+//            swerve.resetPigeonCommand()
+//        );
         /*OI.getInstance().driverController().B_BUTTON.whileTrue(
             AlgaeIntakeSubsystem.getInstance().intake(() -> 0.7)
         );
@@ -64,16 +96,17 @@ public class RobotContainer {
         );*/
 
         /** Resets Pose to desired pose set by dashboard */
-        OI.getInstance().driverController().RIGHT_STICK.whileTrue(
-            swerve.run(() -> 
-                swerve.resetPose(
-                    swerve.getDashboardSelectedResetPose()
-                )
-            )
-        );
+//        OI.getInstance().driverController().RIGHT_STICK.whileTrue(
+//            swerve.run(() ->
+//                swerve.resetPose(
+//                    swerve.getDashboardSelectedResetPose()
+//                )
+//            )
+//        );
     }
 
     public Command getAutonomousCommand() {
-        return autoChooser.getSelected();
+//        return autoChooser.getSelected();
+        return null;
     }
 }
